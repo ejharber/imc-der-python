66824259446c8486fe3491c4fb43a42d2ad376c6
ejharber <ejharber@gmail.com>
2023-03-08 22:14:02 -0800

    updated collision testing

--- test_collision.py
+++ test_collision.py
@@ -1,19 +1,7 @@
 from rope import Rope
 import numpy as np 
 
-rope = Rope(True, 2)
-
-for i in range(10000):
-    rope.stepVel([-.1, 0, 0])
-    if i % 100 == 0:
-        rope.updateRender()
-print(rope.getState().x)
-print(rope.getState().u)
-print(rope.getState().d1)
-print(rope.getState().d2)
-print(rope.getState().tangent)
-print(rope.getState().ref_twist)
-
+rope = Rope(True, 20)
 
 for i in range(1000):
     rope.stepVel([0, 0, 2])
