4a92507974bbdec86d60389e734f521ac46dce1b
ejharber <ejharber@gmail.com>
2023-12-26 04:02:13 -0800

    lstm hyper parameter tuning

--- 2d/Supervized/iterative/model_lstm.py
+++ 2d/Supervized/iterative/model_lstm.py
@@ -3,40 +3,46 @@ from torch import nn
 import numpy as np
 
 class LSTM(nn.Module):
-    def __init__(self, input_dim, num_lstm_layers, hidden_dim, train):
+    def __init__(self, input_dim, lstm_num_layers, lstm_hidden_size, mlp_num_layers, mlp_hidden_size, train):
         super(LSTM, self).__init__()
-        self.lstm_hidden_size = 50
-        self.hidden_dim = hidden_dim + 3
+        self.lstm_num_layers = lstm_num_layers
+        self.lstm_hidden_size = lstm_hidden_size
+        self.mlp_num_layers = mlp_num_layers
+        self.mlp_hidden_size = mlp_hidden_size
+
         self.train = train
 
-        self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim, num_layers=4, batch_first=self.train)
+        self.lstm = nn.LSTM(input_size=input_dim, hidden_size=self.lstm_hidden_size, num_layers=self.lstm_num_layers, batch_first=self.train)
 
         # The linear layer that maps from hidden state space to tag space
-        self.mlp = nn.Sequential(nn.Linear(self.hidden_dim, self.hidden_dim * 4),
-                                 nn.ReLU(),
-                                 nn.Linear(self.hidden_dim * 4, self.hidden_dim * 4),
-                                 nn.ReLU(),
-                                 nn.Linear(self.hidden_dim * 4, 2))
-                                  # nn.ReLU(),
-                                 # nn.Linear(self.hidden_dim * 2, 2))
+        self.mlp = []
+
+        for i in range(self.mlp_num_layers):
+            input_size = self.mlp_hidden_size
+            if i == 0:
+                input_size = self.lstm_hidden_size + 3
+
+            output_size = self.mlp_hidden_size
+            if i == self.mlp_num_layers - 1:
+                output_size = 2
+
+            self.mlp.append(nn.Linear(input_size, output_size))
 
     def forward(self, obs, delta_action):
-        obs = obs.permute(0, 2, 1) 
 
-        h0 = torch.zeros(4, obs.size(0), self.lstm_hidden_size).to('cpu') 
-        c0 = torch.zeros(4, obs.size(0), self.lstm_hidden_size).to('cpu')
+        obs = obs.permute(0, 2, 1) 
+        h0 = torch.zeros(self.lstm_num_layers, obs.size(0), self.lstm_hidden_size).to('cpu') 
+        c0 = torch.zeros(self.lstm_num_layers, obs.size(0), self.lstm_hidden_size).to('cpu')
 
         lstm_out, _ = self.lstm(obs, (h0, c0))
-        # print(lstm_out.shape)
-        # print(delta_action.shape)
-
-
+      
         if self.train:
             mlp_in = torch.cat((lstm_out[:, -1, :], delta_action), axis=1)
         else:
-            mlp_in = torch.append((lstm_out[:, -1, :], delta_action), axis=0)
+            mlp_in = torch.cat((lstm_out[-1, :], delta_action), axis=0)
 
-        # print(mlp_in.shape)
-        mlp_out = self.mlp(mlp_in)
+        for i in range(self.mlp_num_layers):
+            mlp_out = self.mlp[i](mlp_in)
+            mlp_in = nn.functional.relu(mlp_out) # we don't want to apply relu on the last layers
 
         return mlp_out
--- 2d/Supervized/iterative/train_lstm.py
+++ 2d/Supervized/iterative/train_lstm.py
@@ -9,107 +9,137 @@ from torch import nn
 from torch import optim
 from model_lstm import *
 
-# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-device = "cpu"
-# Assuming that we are on a CUDA machine, this should print a CUDA device:
-input_dim, num_lstm_layers, hidden_dim, train = 42, 2, 50, True
+def load_data():
+    file = "rope_motion_m=0_2.npz"
+    data = np.load('data/' + file)
 
+    # collect data
+    X_0 = data["actions_1"]
+    y_0 = np.array(data["traj_pos_1"])[:,-2:,-1]
 
-print(device)
-file = "rope_motion_m=0_2.npz"
-data = np.load('data/' + file)
+    X = np.empty((0, 3))
+    traj_data = np.empty((0, 42, 101))
+    y = np.empty((0, 2))
 
-X_0 = data["actions_1"]
-y_0 = np.array(data["traj_pos_1"])[:,-2:,-1]
+    for i in range(2, 10):
+        X_ = data["actions_" + str(i)] - X_0 # delta action 
+        traj_pos_, traj_force_ = data["traj_pos_" + str(i-1)], data["traj_force_" + str(i-1)]
+        y_ = data["traj_pos_" + str(i)][:,-2:,-1] - y_0 # delta goal 
 
-X = np.empty((0, 3))
-traj_data = np.empty((0, 42, 101))
-y = np.empty((0, 2))
+        traj_data_ = np.append(traj_pos_, traj_force_, axis = 1)
 
-for i in range(2, 10):
-    X_ = data["actions_" + str(i)] - X_0 # delta action 
-    traj_pos_, traj_force_ = data["traj_pos_" + str(i-1)], data["traj_force_" + str(i-1)]
-    y_ = data["traj_pos_" + str(i)][:,-2:,-1] - y_0 # delta goal 
+        X = np.append(X, X_, axis = 0)
+        traj_data = np.append(traj_data, traj_data_, axis = 0)
+        y = np.append(y, y_, axis = 0)
 
-    # gaussian_noise = np.random.normal(mean, std_deviation, shape)
-    # gaussian_noise = np.random.normal(mean, std_deviation, shape)
+    # add artifical noise (similar to that as we would expect from the sensors)
+    traj_data[:, :40, :] = np.random.normal(traj_data[:, :40, :], 0.003, traj_data[:, :40, :].shape) # artificial mocap noise added
+    traj_data[:, 40:, :] = np.random.normal(traj_data[:, 40:, :], 0.15, traj_data[:, 40:, :].shape) # artificial force sensor noise added
 
-    traj_data_ = np.append(traj_pos_, traj_force_, axis = 1)
-    # print(traj_pos_.shape, traj_force_.shape, traj_data_.shape, X.shape, X_.shape)
+    # normalize data
+    traj_mean_pos = np.mean(traj_data[:, :40, :])
+    traj_std_pos = np.std(traj_data[:, :40, :])
+    traj_data[:, :40, :] = traj_data[:, :40, :] - traj_mean_pos
+    traj_data[:, :40, :] = traj_data[:, :40, :] / traj_std_pos
 
-    X = np.append(X, X_, axis = 0)
-    traj_data = np.append(traj_data, traj_data_, axis = 0)
-    y = np.append(y, y_, axis = 0)
+    traj_mean_force = np.mean(traj_data[:, 40:, :])
+    traj_std_force = np.std(traj_data[:, 40:, :])
+    traj_data[:, 40:, :] = traj_data[:, 40:, :] - traj_mean_force
+    traj_data[:, 40:, :] = traj_data[:, 40:, :] / traj_std_force
 
-print(X.shape, traj_data.shape, y.shape)
+    X_mean = np.mean(X)
+    X_std = np.std(X)
+    X = X - np.mean(X)
+    X = X / np.std(X)
 
-X = torch.from_numpy(X.astype(np.float32)).to(device)
-traj_data = torch.from_numpy(traj_data.astype(np.float32)).to(device)
-y = torch.from_numpy(y.astype(np.float32)).to(device)
+    y_mean = np.mean(y)
+    y_std = np.std(y)
+    y = y - y_mean
+    y = y / y_std
 
-print(X.shape, traj_data.shape, y.shape)
+    # print(traj_mean_pos, traj_std_pos, traj_mean_force, traj_std_force, X_mean, X_std, y_mean, y_std)
 
-rand_i = np.linspace(0, X.shape[0] - 1, X.shape[0], dtype = int)
-np.random.shuffle(rand_i)
-split = X.shape[0] * 2 // 3
+    return X, traj_data, y
 
-X_train, X_test = X[rand_i[:split], :], X[rand_i[split:], :]
-traj_data_train, traj_data_test = traj_data[rand_i[:split], :, :], traj_data[rand_i[split:], :, :]
-y_train, y_test = y[rand_i[:split], :], y[rand_i[split:], :]
+def train_lstm(include_force, include_pos, lstm_num_layers, lstm_hidden_size, mlp_num_layers, mlp_hidden_size):
+    # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    device = "cpu" 
 
-model = LSTM(input_dim, num_lstm_layers, hidden_dim, train)
-model.to(device)
+    if include_force and include_pos:
+        save_file_name = "alldata_" + str(lstm_num_layers) + "_" + str(lstm_hidden_size) + "_" + str(mlp_num_layers) + "_" + str(mlp_hidden_size)
+    elif not include_force:
+        save_file_name = "noforce_" + str(lstm_num_layers) + "_" + str(lstm_hidden_size) + "_" + str(mlp_num_layers) + "_" + str(mlp_hidden_size)
+    elif not include_pos:
+        save_file_name = "nopose_" + str(lstm_num_layers) + "_" + str(lstm_hidden_size) + "_" + str(mlp_num_layers) + "_" + str(mlp_hidden_size)
 
-learning_rate = .2
-loss_fn = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+    input_dim = 42
+    if not include_force:
+        traj_data = traj_data[:, :40, :]
+        input_dim = 40
 
-num_epochs = 1000
-loss_values_train = []
-loss_values_test = []
-for epoch in range(num_epochs):
+    if not include_pos:
+        traj_data = traj_data[:, 40:, :]
+        input_dim = 2
 
-    last = 0
-    for batch in np.linspace(0, X_train.shape[0], 10, endpoint=False, dtype=np.int32):
-        if batch == 0: continue 
+    X, traj_data, y = load_data()
 
-        # print(batch)
+    # begin training
+    print(X.shape, traj_data.shape, y.shape)
 
-        # print("training", X_train.shape)
-        # zero the parameter gradients
-        optimizer.zero_grad()
-        
-        # forward + backward + optimize
-        pred = model(traj_data_train[last:batch, :, :], X_train[last:batch, :])
-        loss = loss_fn(pred, y_train[last:batch, :])
-        loss_values_train.append(loss.item())
-        loss.backward()
-        optimizer.step()
+    X = torch.from_numpy(X.astype(np.float32)).to(device)
+    traj_data = torch.from_numpy(traj_data.astype(np.float32)).to(device)
+    y = torch.from_numpy(y.astype(np.float32)).to(device)
 
-        last = batch
+    rand_i = np.linspace(0, X.shape[0] - 1, X.shape[0], dtype = int)
+    np.random.shuffle(rand_i)
+    split = X.shape[0] * 2 // 3
 
-    pred = model(traj_data_test, X_test)
-    loss = loss_fn(pred, y_test)
-    loss_values_test.append(loss.item())
-    print("no force", epoch, loss_values_test[-1], loss_values_train[-1])
+    X_train, X_test = X[rand_i[:split], :], X[rand_i[split:], :]
+    traj_data_train, traj_data_test = traj_data[rand_i[:split], :, :], traj_data[rand_i[split:], :, :]
+    y_train, y_test = y[rand_i[:split], :], y[rand_i[split:], :]
 
-torch.save(model.state_dict(), "iterative_delta_force_foward_dynamics")
+    model = LSTM(input_dim, lstm_num_layers, lstm_hidden_size, mlp_num_layers, mlp_hidden_size, train = True)
+    model.to(device)
 
-print("Training Complete")
+    learning_rate = 0.1
+    loss_fn = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-step = np.linspace(0, num_epochs, len(np.array(loss_values_train)))
+    num_epochs = 1000
+    loss_values_train = []
+    loss_values_test = []
+    for epoch in range(num_epochs):
 
-fig, ax = plt.subplots(figsize=(8,5))
-plt.plot(step, np.array(loss_values_train))
-plt.title("Step-wise Loss")
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
+        last = 0
+        for batch in np.linspace(0, X_train.shape[0], 10, endpoint=False, dtype=np.int32):
+            if batch == 0: continue 
 
-plt.figure()
-plt.plot(loss_values_test)
-plt.title("Step-wise Loss")
-plt.xlabel("Epochs")
-plt.ylabel("Loss")
+            # print(batch)
 
-plt.show()
+            # print("training", X_train.shape)
+            # zero the parameter gradients
+            optimizer.zero_grad()
+            
+            # forward + backward + optimize
+            pred = model(traj_data_train[last:batch, :, :], X_train[last:batch, :])
+            loss = loss_fn(pred, y_train[last:batch, :])
+            loss_values_train.append(loss.item())
+            # print(loss.item())
+            loss.backward()
+            optimizer.step()
 
+            last = batch
+
+        pred = model(traj_data_test, X_test)
+        loss = loss_fn(pred, y_test)
+        loss_values_test.append(loss.item())
+        print(epoch, loss_values_test[-1], loss_values_train[-1])
+
+    np.savez(save_file_name, loss_values_test = loss_values_test, loss_values_train = loss_values_train)
+    torch.save(model.state_dict(), save_file_name)
+
+include_pos = True
+include_force = True
+lstm_num_layers, lstm_hidden_size, mlp_num_layers, mlp_hidden_size = 2, 100, 4, 512
+
+train_lstm(include_force, include_pos, lstm_num_layers, lstm_hidden_size, mlp_num_layers, mlp_hidden_size)
